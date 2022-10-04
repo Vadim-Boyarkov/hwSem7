fe730b4af7220de0522b6af1cdfb66840597a17e
Vadim-Boyarkov <fluentzhiv@gmail.com>
2022-10-04 22:14:53 +0500

    all

---
+++ 47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int Read(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double[,] ReadMatrix(int rowsCount, int columnsCount, double leftRange = -50.0, double rightRange = 50.0)
+{
+    double[,] matrix = new double[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rand.NextDouble()* (rightRange - leftRange) + leftRange , 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+     Console.WriteLine("Полученный двумерный массив:");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = Read("Введите кол-во строк");
+int n = Read("Введите кол-во столбцов");
+double[,] matr = ReadMatrix(m, n);
+PrintMatrix(matr);
---
+++ 50/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+int Read(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double[,] ReadMatrix(int rowsCount, int columnsCount, double leftRange = -10.0, double rightRange = 10.0)
+{
+    double[,] matrix = new double[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rand.NextDouble() * (rightRange - leftRange) + leftRange, 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    Console.WriteLine("Полученный двумерный массив:");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+void SearchElement(double[,] matrix, int line, int column)
+{
+    if (line < 1|| line > matrix.GetLength(0) || column < 1 || column > matrix.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента не существует!");
+    }
+    else
+        Console.Write($"Искомый элемент = {matrix[line -1, column -1]}");
+}
+int m = Read("Введите кол-во строк");
+int n = Read("Введите кол-во столбцов");
+double[,] matr = ReadMatrix(m, n);
+PrintMatrix(matr);
+int line = Read("Введите строку необходимого элемента");
+int column = Read("Введите столбец необходимого элемента");
+SearchElement(matr, line, column);
